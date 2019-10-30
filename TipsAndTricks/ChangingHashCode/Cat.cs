d5f0ea9a0412ba3ddeef5b3c6356906e20a11948
u.ashikov <u.ashikov@abv.bg>
2019-10-31 00:15:59 +0200

    ChangingHashCode example.

---
+++ TipsAndTricks/ChangingHashCode/Cat.cs
@@ -0,0 +1,23 @@
+﻿namespace ChangingHashCode
+{
+    public class Cat
+    {
+        public Cat(int id)
+        {
+            this.Id = id;
+        }
+
+        public int Id { get; }
+
+        public string Name { get; set; }
+
+        public override int GetHashCode()
+        {
+            // Wrong, if any of the properties, that are included in the calculation can be changed from outside or from logic inside the class.
+            //return this.Id.GetHashCode() ^ this.Name.GetHashCode();
+
+            // Use the read only property to generate the hash code, so no one can change it.
+            return this.Id.GetHashCode();
+        }
+    }
+}
---
+++ TipsAndTricks/ChangingHashCode/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChangingHashCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var catOne = new Cat(1) { Name = "Gosho" };
+            var catTwo = new Cat(2) { Name = "Pesho" };
+            var catThree = new Cat(3) { Name = "Gergan" };
+            var catFour = new Cat(4) { Name = "Stamat" };
+
+            // Cat - Owner
+            var cats = new Dictionary<Cat, string>
+            {
+                { catOne, "Ivan" },
+                { catTwo, "Dragan" },
+                { catThree, "Slavcho" },
+                { catFour, "Petko" }
+            };
+
+            var fourthCatOwner = cats[catFour];
+
+            Console.WriteLine($"Fourth cat owner's name is {fourthCatOwner}");
+
+            catFour.Name = "Mincho";
+
+            fourthCatOwner = cats[catFour];
+
+            Console.WriteLine($"Fourth cat owner's name is {fourthCatOwner}");
+        }
+    }
+}
