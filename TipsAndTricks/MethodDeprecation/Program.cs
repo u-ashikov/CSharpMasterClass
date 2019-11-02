d9e4cc5383afcf5a884a6922f14c3d930d1b9bc6
u.ashikov <u.ashikov@abv.bg>
2019-11-02 22:28:33 +0200

    Method deprecation example with obsolete attribute.

---
+++ TipsAndTricks/MethodDeprecation/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MethodDeprecation
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var writer = new TextWriter();
+
+            writer.WriteToFile("text.txt", "Hello world.");
+        }
+    }
+}
---
+++ TipsAndTricks/MethodDeprecation/TextWriter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace MethodDeprecation
+{
+    public class TextWriter
+    {
+        //[Obsolete("Please use WriteToFile instead.")]
+        [Obsolete("Please use WriteToFile instead.", true)]
+        public void WriteToFile(string pathToFile, string text)
+        {
+            if (File.Exists(pathToFile))
+            {
+                File.WriteAllText(pathToFile, text);
+            }
+        }
+
+        public void WriteToConsole(string text)
+        {
+            Console.WriteLine($"Your text is: {text}");
+        }
+    }
+}
