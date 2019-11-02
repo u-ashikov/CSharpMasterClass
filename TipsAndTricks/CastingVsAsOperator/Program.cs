fd4728f5d7adf94b3b8b4eef40a5b346da170c0a
u.ashikov <u.ashikov@abv.bg>
2019-11-02 22:12:29 +0200

    Casting vs as operator example.

---
+++ TipsAndTricks/CastingVsAsOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CastingVsAsOperator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            object number = 10;
+            //string numberAsString = (string)number;
+
+            // InvalidCastException will be thrown.
+            //Console.WriteLine(numberAsString);
+
+            // The number will be null, ant won't throw exception.
+            string numberAsString = number as string;
+            Console.WriteLine(numberAsString);
+        }
+    }
+}
