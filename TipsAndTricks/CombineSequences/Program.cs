d11af0916934e7882fb8d6d0e7bb91af31f39598
u.ashikov <u.ashikov@abv.bg>
2019-11-03 14:12:28 +0200

    Combining sequences examples.

---
+++ TipsAndTricks/CombineSequences/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CombineSequences
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var firstList = new List<string>() { "apple", "banana", "cherry", "dragon fruit", "eggplant" };
+            var secondList = new List<string>() { "banana", "cherry", "dragon fruit", "mushroom" };
+
+            Console.WriteLine("===== Concatenate =====");
+            var concatenatedList = firstList.Concat(secondList);
+            Console.WriteLine(string.Join(", ", concatenatedList));
+            Console.WriteLine();
+
+            Console.WriteLine("===== Intersect =====");
+            var intersectedList = firstList.Intersect(secondList);
+            Console.WriteLine(string.Join(", ", intersectedList));
+            Console.WriteLine();
+
+            Console.WriteLine("===== Except =====");
+            var exceptList = firstList.Except(secondList);
+            Console.WriteLine(string.Join(", ", exceptList));
+            Console.WriteLine();
+
+            Console.WriteLine("===== Union =====");
+            var unionList = firstList.Union(secondList);
+            Console.WriteLine(string.Join(", ", unionList));
+            Console.WriteLine();
+        }
+    }
+}
