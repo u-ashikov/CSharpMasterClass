4ed87efa7608bcb8acb331aca561107ced0b8754
u.ashikov <u.ashikov@abv.bg>
2019-11-03 14:03:52 +0200

    Optional parameters example.

---
+++ TipsAndTricks/OptionalParameters.Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using OptionalParameters.Library;
+using System;
+
+namespace OptionalParameters.Client
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Change the optional parameter amount in the library project with another value an rebuild only the library project.
+            // Run the client project in Power Shell and you will see that the anotherBankAccount has amount different than the newly set amount in the library. It will hold the previous amount.
+
+            var bankAccount = new BankAccount("Mincho Minchev", 250);
+            bankAccount.PrintInfo();
+
+            Console.WriteLine();
+
+            var anotherBankAccount = new BankAccount("Gergan Gerganov");
+            anotherBankAccount.PrintInfo();
+        }
+    }
+}
---
+++ TipsAndTricks/OptionalParameters.Library/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OptionalParameters.Library
+{
+    public class BankAccount
+    {
+        public BankAccount(string accountHolder, decimal amount = 999)
+        {
+            this.AccountHolder = accountHolder;
+            this.Amount = amount;
+        }
+
+        public string AccountHolder { get; }
+        public decimal Amount { get; }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Bank Account Info: Holder: {this.AccountHolder}, Amount: {this.Amount.ToString("0.00")} $");
+        }
+    }
+}
