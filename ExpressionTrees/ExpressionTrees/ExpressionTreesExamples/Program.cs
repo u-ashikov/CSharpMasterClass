c66b4476bdf4d2756b0816f725b658c416ff08b6
u.ashikov <u.ashikov@abv.bg>
2019-11-16 23:37:43 +0200

    Expression trees project.

---
+++ ExpressionTrees/ExpressionTrees/ExpressionTreesExamples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpressionTreesExamples
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // 1. Example
+            Expression<Func<int>> constant = () => 42;
+        }
+    }
+}
