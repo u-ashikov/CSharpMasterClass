f360d0a7e13294bcba3597693919fa69bbc40c7c
u.ashikov <u.ashikov@abv.bg>
2019-11-21 22:59:15 +0200

    Exception handling in threads - the wrong way.

---
+++ Multithreading/ExceptionHandlingInThreads/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace ExceptionHandlingInThreads
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                var thread = new Thread(MethodWithException);
+                thread.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("I have caught the exception!");
+            }
+        }
+
+        public static void MethodWithException()
+        {
+            throw new ArgumentNullException();
+        }
+    }
+}
