ed57b67ae0ad50364cd3e83f8935ce3b484a9371
u.ashikov <u.ashikov@abv.bg>
2019-11-23 23:30:58 +0200

    Semaphores example.

---
+++ Multithreading/Semaphores/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Semaphores
+{
+    public class Program
+    {
+        public static Semaphore semaphore = new Semaphore(4, 4);
+
+        public static void Main()
+        {
+            while (true)
+            {
+                Thread.Sleep(300);
+                new Thread(DoWork).Start();
+            }
+        }
+
+        public static void DoWork()
+        {
+            semaphore.WaitOne();
+            Console.WriteLine("I am working " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(2000);
+            Console.WriteLine("End of work :) " + Thread.CurrentThread.ManagedThreadId);
+            semaphore.Release();
+        }
+    }
+}
