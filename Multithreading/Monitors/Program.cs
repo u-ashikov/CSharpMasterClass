ff64417e4cb223e0d9e452d3f63c51124578c947
u.ashikov <u.ashikov@abv.bg>
2019-11-23 22:55:09 +0200

    Using monitor pulse and monitor wait.

--- Multithreading/Monitors/Program.cs
+++ Multithreading/Monitors/Program.cs
@@ -13,19 +13,25 @@ namespace Monitors
 
         public static void Main()
         {
-            var threads = new List<Thread>();
+            //var threads = new List<Thread>();
 
-            for (int i = 0; i < 4; i++)
-            {
-                var thread = new Thread(IncrementNumber);
-                threads.Add(thread);
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    var thread = new Thread(IncrementNumber);
+            //    threads.Add(thread);
 
-                thread.Start();
-            }
+            //    thread.Start();
+            //}
 
-            threads.ForEach(t => t.Join());
+            //threads.ForEach(t => t.Join());
 
-            Console.WriteLine(Number);
+            //Console.WriteLine(Number);
+
+            new Thread(PrepareWork).Start();
+
+            Thread.Sleep(300);
+
+            new Thread(DoWorkFromWorkers).Start();
         }
 
         public static void IncrementNumber()
@@ -38,6 +44,32 @@ namespace Monitors
             }
 
             Monitor.Exit(obj);
+        }
+
+        public static void PrepareWork()
+        {
+            while (true)
+            {
+                Monitor.Enter(obj);
+                Thread.Sleep(1000);
+                Console.WriteLine("I am preparing a job for you!");
+                Monitor.Wait(obj);
+                Console.WriteLine("I am preparing again new task for you!");
+                Monitor.Exit(obj);
+            }
+        }
+
+        public static void DoWorkFromWorkers()
+        {
+            while (true)
+            {
+                Monitor.Enter(obj);
+                Console.WriteLine("Work in progress....");
+                Thread.Sleep(1000);
+                Console.WriteLine("We are done!");
+                Monitor.Pulse(obj);
+                Monitor.Exit(obj);
+            }
         }
     }
 }
